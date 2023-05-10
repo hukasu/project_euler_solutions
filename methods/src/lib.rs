d089d814423f36d410fce4a31ef772b8e4b6331c
Lucas Farias <lucasfnf@gmail.com>
2023-05-10 19:04:13 +0900

    Move methods of Problem 1 into common library package

---
+++ methods/src/lib.rs
@@ -0,0 +1,5 @@
+/// Checks if number is multiple of either `3` or `5`.
+pub fn multiple_of_3_or_5(x: &u64) -> bool {
+    (x % 3 == 0) || (x % 5 == 0)
+}
+
--- p0001/src/main.rs
+++ p0001/src/main.rs
@@ -1,6 +1,4 @@
-fn multiple_of_3_or_5(x: &u64) -> bool {
-    (x % 3 == 0) || (x % 5 == 0)
-}
+use methods::multiple_of_3_or_5;
 
 fn main() {
     let s: u64 = (1_u64..1000)
