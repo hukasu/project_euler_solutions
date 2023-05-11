e94619b41a99a2f336ca3297cb582f73bd4fe724
Lucas Farias <lucasfnf@gmail.com>
2023-05-11 15:55:14 +0900

    Cargo fmt fixes

--- p0001/src/main.rs
+++ p0001/src/main.rs
@@ -1,8 +1,6 @@
 use methods::multiple_of_3_or_5;
 
 fn main() {
-    let s: u64 = (1_u64..1000)
-        .filter(multiple_of_3_or_5)
-        .sum();
+    let s: u64 = (1_u64..1000).filter(multiple_of_3_or_5).sum();
     println!("{}", s);
 }
