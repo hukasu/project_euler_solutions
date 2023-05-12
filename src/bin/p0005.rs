6dc9f33e2589f6ef331069ca9921fbf16a5d1b10
Lucas Farias <lucasfnf@gmail.com>
2023-05-12 19:43:08 +0900

    Move tests relevant to Problem 5 to its file

--- src/bin/p0005.rs
+++ src/bin/p0005.rs
@@ -22,3 +22,26 @@ fn main() {
         });
     println!("{r:?}");
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    fn prime_factor_frequencies_test() {
+        assert_eq!(get_prime_factors_frequencies(2), HashMap::from([(2, 1)]));
+        assert_eq!(get_prime_factors_frequencies(3), HashMap::from([(3, 1)]));
+        assert_eq!(get_prime_factors_frequencies(4), HashMap::from([(2, 2)]));
+        assert_eq!(get_prime_factors_frequencies(5), HashMap::from([(5, 1)]));
+        assert_eq!(
+            get_prime_factors_frequencies(6),
+            HashMap::from([(2, 1), (3, 1)])
+        );
+        assert_eq!(get_prime_factors_frequencies(16), HashMap::from([(2, 4)]));
+        assert_eq!(
+            get_prime_factors_frequencies(20),
+            HashMap::from([(2, 2), (5, 1)])
+        );
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -404,23 +404,6 @@ mod tests {
     }
 
     #[test]
-    fn prime_factor_frequencies_test() {
-        assert_eq!(get_prime_factors_frequencies(2), HashMap::from([(2, 1)]));
-        assert_eq!(get_prime_factors_frequencies(3), HashMap::from([(3, 1)]));
-        assert_eq!(get_prime_factors_frequencies(4), HashMap::from([(2, 2)]));
-        assert_eq!(get_prime_factors_frequencies(5), HashMap::from([(5, 1)]));
-        assert_eq!(
-            get_prime_factors_frequencies(6),
-            HashMap::from([(2, 1), (3, 1)])
-        );
-        assert_eq!(get_prime_factors_frequencies(16), HashMap::from([(2, 4)]));
-        assert_eq!(
-            get_prime_factors_frequencies(20),
-            HashMap::from([(2, 2), (5, 1)])
-        );
-    }
-
-    #[test]
     fn multiple_of_all_test() {
         assert_eq!(smallest_multiple_of_all_through_x(10), 2520);
     }
