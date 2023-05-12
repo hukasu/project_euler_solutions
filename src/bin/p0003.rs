c79038ce9eb1171115a3ddfc4d236883fe05c2f8
Lucas Farias <lucasfnf@gmail.com>
2023-05-12 19:39:27 +0900

    Move tests relevant to Problem 3 to its files

--- src/bin/p0003.rs
+++ src/bin/p0003.rs
@@ -3,3 +3,19 @@ use project_euler::get_prime_factors;
 fn main() {
     println!("{:?}", get_prime_factors(600851475143));
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn prime_factor_test() {
+        assert_eq!(get_prime_factors(2), vec![]);
+        assert_eq!(get_prime_factors(3), vec![]);
+        assert_eq!(get_prime_factors(4), vec![2]);
+        assert_eq!(get_prime_factors(5), vec![]);
+        assert_eq!(get_prime_factors(6), vec![2, 3]);
+        assert_eq!(get_prime_factors(13195), vec![5, 7, 13, 29]);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -393,16 +393,6 @@ mod tests {
     use crate::*;
 
     #[test]
-    fn prime_factor_test() {
-        assert_eq!(get_prime_factors(2), vec![]);
-        assert_eq!(get_prime_factors(3), vec![]);
-        assert_eq!(get_prime_factors(4), vec![2]);
-        assert_eq!(get_prime_factors(5), vec![]);
-        assert_eq!(get_prime_factors(6), vec![2, 3]);
-        assert_eq!(get_prime_factors(13195), vec![5, 7, 13, 29]);
-    }
-
-    #[test]
     fn factor_test() {
         assert_eq!(get_factors(2), vec![]);
         assert_eq!(get_factors(3), vec![]);
