68081c3e33402d68a243245bba71e3f72bddcc8f
Lucas Farias <lucasfnf@gmail.com>
2023-05-20 09:56:01 +0900

    Cargo fmt on Problem 36

--- src/bin/p0036.rs
+++ src/bin/p0036.rs
@@ -1,6 +1,8 @@
 use project_euler::is_palindrome;
 
 fn main() {
-    let r = (0..=1_000_000).filter(|n| is_palindrome(&n.to_string()) && is_palindrome(&format!("{:b}", n))).sum::<u64>();
+    let r = (0..=1_000_000)
+        .filter(|n| is_palindrome(&n.to_string()) && is_palindrome(&format!("{:b}", n)))
+        .sum::<u64>();
     println!("{r:?}");
 }
