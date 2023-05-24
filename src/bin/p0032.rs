a5c7e7565902ac252a312ee02c3a9a8bead80d7b
Lucas Farias <lucasfnf@gmail.com>
2023-05-24 15:21:25 +0900

    Fix filtering out values introduced by the generalization of `pandigital_numbers`

--- src/bin/p0032.rs
+++ src/bin/p0032.rs
@@ -5,6 +5,7 @@ use project_euler::pandigital_numbers;
 fn main() {
     let r = (1..=100)
         .flat_map(|a| ((a + 1)..=10000).map(|b| [a * b, a, b]).collect::<Vec<_>>())
+        .filter(|ns| ns.iter().map(u64::to_string).collect::<String>().len() == 9)
         .filter(|ns| pandigital_numbers(ns))
         .map(|[p, mp, ms]| (p, (mp, ms)))
         .collect::<BTreeMap<u64, (u64, u64)>>()
